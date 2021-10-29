037b0479a06ce8591ff35f3bc11e4e00d948a63a
Andrew Brown <algorithmicmusic@gmail.com>
2021-10-29 12:33:29 +1000

    Cast ESP32 comm format to allow compile

--- M16.h
+++ M16.h
@@ -75,7 +75,7 @@ static const i2s_config_t i2s_config = {
     .sample_rate = SAMPLE_RATE,
     .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
     .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
-    .communication_format = I2S_COMM_FORMAT_STAND_I2S,
+    .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB), //I2S_COMM_FORMAT_STAND_I2S,
     .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,       // high interrupt priority
     .dma_buf_count = 8,                             // 8 buffers
     .dma_buf_len = 64, //1024,                            // 1K per buffer, so 8K of buffer space
@@ -86,9 +86,9 @@ static const i2s_config_t i2s_config = {
 
 /* ESP32 I2S pin allocation */
 static const i2s_pin_config_t pin_config = { // D1 mini, NodeMCU
-    .bck_io_num = 25, //25, //25,     // 25    // 8   // 6                     // The bit clock connectiom, goes to pin 27 of ESP32
-    .ws_io_num = 27, //27, //27,       //27    // 6   // 4                // Word select, also known as word select or left right clock
-    .data_out_num = 12, //12, //12,     //26   // 7   // 5            // Data out from the ESP32, connect to DIN on 38357A
+    .bck_io_num = 25, //25,     // 25    // 8   // 6                     // The bit clock connectiom, goes to pin 27 of ESP32
+    .ws_io_num = 27, //27,      //27    // 6   // 4                // Word select, also known as word select or left right clock
+    .data_out_num = 12, //12,     //26   // 7   // 5            // Data out from the ESP32, connect to DIN on 38357A
     .data_in_num = I2S_PIN_NO_CHANGE                // we are not interested in I2S data into the ESP32
 };
 
