007ce9cef2b2fbe94ba709fa0440f4be3dc08c45
Andrew Brown <algorithmicmusic@gmail.com>
2025-02-11 16:20:53 +1000

    refactored

--- All.h
+++ All.h
@@ -5,7 +5,8 @@
  *
  * by Andrew R. Brown 2025
  *
- * This filter is based on the allpass~ object in Max
+ * This filter is based on the allpass~ object in Max 
+ * with thanks to Derek Kwan's implementation in Cylone for Pure Data
  * 
  * This file is part of the M16 audio library.
  *
@@ -22,7 +23,7 @@ class All {
     All() {}
 
     /** Constructor */
-    All(int32_t delay, float feedback) {
+    All(float delay, float feedback) {
         setDelayTime(delay);
         setFeedbackLevel(feedback);
     }
@@ -32,22 +33,31 @@ class All {
      *  Not technically a state variable filter, but...
      */
     inline
-    int16_t next(int32_t input) {
+    int next(int input) {
       // y[n] = (-g * x[n]) + x[n - d] + (g * y[n - d]) // g = gain, d = delay, x = input, y = output
       // set up first time called
       if (!allpassInitiated) {
         initAllpass();
       }
-      int32_t inG = -feedbackLevel * input;
-      int bufferReadIndex = (bufferIndex - delayTime_samples);
-      if (bufferReadIndex < 0) bufferReadIndex += bufferSize_samples; 
-      int32_t inMinusD = inputBuffer[bufferReadIndex];
-      int32_t outGminusD = (feedbackLevel * outputBuffer[bufferReadIndex])>>10; 
-      int32_t output = clip16(inG + inMinusD + outGminusD);
-      // update buffers
-      inputBuffer[bufferIndex] = input;
-      outputBuffer[bufferIndex] = output;
-      bufferIndex = (bufferIndex + 1) % bufferSize_samples;
+      //write input to delay buffer
+      inputBuffer[bufferWriteIndex] = input;
+      // increment read index
+      bufferReadIndex = bufferReadIndex + 1; 
+      if (bufferReadIndex >= bufferSize_samples) {
+        bufferReadIndex = 0;
+      }
+      //get delayed values of x and y
+      int delX = inputBuffer[bufferReadIndex];
+      int delY = outputBuffer[bufferReadIndex]; 
+      //figure out your current y term: y[n] = -a*x[n] + x[n-d] + a*y[n-d]
+      int output = clip16(((feedbackLevel * -1 * input)>>10) + delX + ((feedbackLevel * delY)>>10));
+      //stick the output in the ybuffer
+      outputBuffer[bufferWriteIndex] = output;
+      //increment write index
+      bufferWriteIndex = bufferWriteIndex + 1; 
+      if (bufferWriteIndex >= bufferSize_samples) {
+        bufferWriteIndex = 0;
+      }
       return output;
     }
 
@@ -57,8 +67,13 @@ class All {
     inline
     void setFeedbackLevel(float level) {
       if (level >= 0 && level <= 1) {
-        feedbackLevel = level * 1024;
+        feedbackLevel = pow(level, 0.4) * 1024;
       } else Serial.println("Feedback level must be between 0 and 1");
+    }
+
+    /** Get the feedback level of the allpass filter */
+    float getFeedbackLevel() {
+      return feedbackLevel * 0.0009765625;
     }
 
     /** Set the allpass filter size
@@ -76,7 +91,7 @@ class All {
      * @param time The length of the delay line in milliseconds
      */
     inline
-    void setDelayTime(int16_t time) {
+    void setDelayTime(float time) {
       if (time >= 0) {
         if (time > allpassSize) {
           allpassSize = time * 10;
@@ -85,26 +100,35 @@ class All {
         delayTime = time;
         delayTime_samples = delayTime * 0.001f * SAMPLE_RATE;
       } else Serial.println("Allpass delay time must be between 0 and allpass size");
+      // set the read index
+      bufferReadIndex = (bufferWriteIndex - delayTime_samples);
+      if (bufferReadIndex < 0) {
+        bufferReadIndex = bufferReadIndex + bufferSize_samples - 1;
+      } else if  (bufferReadIndex >= bufferSize_samples) {
+        bufferReadIndex = bufferReadIndex % bufferSize_samples;
+      }
     }
 
   private:
 
     bool allpassInitiated = false;
     int16_t allpassSize = 100; // in ms 
-    int32_t bufferSize_samples;
-    int16_t delayTime = 1; // in ms // 0 - allpassSize
-    int32_t delayTime_samples;
-    int16_t feedbackLevel = 700; // 0-1024
-    int32_t * inputBuffer;
-    int32_t * outputBuffer;
-    int32_t bufferIndex = 0;
+    int bufferSize_samples;
+    float delayTime = 1; // in ms // 0 - allpassSize
+    int delayTime_samples;
+    int feedbackLevel = 700; // 0-1024
+    int * inputBuffer;
+    int * outputBuffer;
+    int bufferWriteIndex = 0;
+    int bufferReadIndex = 0;
+    int prevOutput = 0;
 
      /** Create the allpass filter input signal buffer */
     void createInputBuffer() {
       delete[] inputBuffer; // remove any previous memory allocation
       bufferSize_samples = allpassSize * 0.001f * SAMPLE_RATE;
       setDelayTime(delayTime);
-      inputBuffer = new int32_t[bufferSize_samples]; // create a new buffer
+      inputBuffer = new int[bufferSize_samples]; // create a new buffer
       for(int i=0; i<bufferSize_samples; i++) {
         inputBuffer[i] = 0; // zero out the buffer
       }
@@ -115,7 +139,7 @@ class All {
       delete[] outputBuffer; // remove any previous memory allocation
       bufferSize_samples = allpassSize * 0.001f * SAMPLE_RATE;
       setDelayTime(delayTime);
-      outputBuffer = new int32_t[bufferSize_samples]; // create a new buffer
+      outputBuffer = new int[bufferSize_samples]; // create a new buffer
       for(int i=0; i<bufferSize_samples; i++) {
         outputBuffer[i] = 0; // zero out the buffer
       }
